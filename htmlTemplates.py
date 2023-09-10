97a3ff4612df4e5ce745bbf9401aa068c7f9ef42
Qingyu Yu <yqy1998@gmail.com>
2023-09-10 05:58:44 -0400

    Whole project

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://as1.ftcdn.net/v2/jpg/03/22/38/32/1000_F_322383277_xcXz1I9vOFtdk7plhsRQyjODj08iNSwB.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://icons.veryicon.com/png/o/miscellaneous/two-color-webpage-small-icon/user-244.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
--- main.py
+++
@@ -1,8 +0,0 @@
-# python-dotenv
-
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print("this comes from .env: ",os.getenv('MY_API_KEY'))
